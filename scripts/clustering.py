aed8fc828dc011141cdbefb5477b06407f6b8e3f
AnnaB <annabazan3@gmail.com>
2025-11-27 00:08:01 +0100

    clustering script small changes

--- scripts/clustering.py
+++ scripts/clustering.py
@@ -7,33 +7,16 @@ import umap
 import matplotlib.pyplot as plt
 import math
 
-# -----------------------------
-# Helpers for parsing columns
-# -----------------------------
-def path_length_points(s):
-    """Number of nodes in the path"""
-    try:
-        # Remove quotes if present
-        s = s.strip('"')
-        nodes = s.split(",")
-        return len(nodes)
-    except:
-        return 0
 
-# -----------------------------
 # Load data
-# -----------------------------
 df = pd.read_csv("../results/enriched_routes/beynes_routes_enriched.csv", quotechar='"')
 
-# -----------------------------
-# Engineer path-based feature
-# -----------------------------
-df['path_points'] = df['path'].apply(path_length_points)
-df = df.drop(columns=['path'])  # drop raw path column
 
-# -----------------------------
+# Drop the path column becauce it has strings
+df = df.drop(columns=['path']) 
+
+
 # Select numeric features for clustering
-# -----------------------------
 numeric_cols = [
     'free_flow_time','num_edges','total_length','mean_speed',
     'pct_paved','pct_unpaved','pct_motorway','pct_trunk','pct_primary',
@@ -49,15 +32,12 @@ features = [col for col in numeric_cols if col in df.columns]
 # Fill missing values
 X = df[features].fillna(0).values
 
-# -----------------------------
+
 # Normalize features
-# -----------------------------
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
-# -----------------------------
 # HDBSCAN clustering
-# -----------------------------
 clusterer = hdbscan.HDBSCAN(
     min_cluster_size=50,
     min_samples=5,
@@ -69,14 +49,12 @@ df["cluster"] = labels
 
 print(df["cluster"].value_counts())
 
-# -----------------------------
+
 # Save clustered dataset
-# -----------------------------
 df.to_csv("../results/clustered_routes/beynes_routes_clustered.csv", index=False)
 
-# -----------------------------
+
 # UMAP visualization
-# -----------------------------
 reducer = umap.UMAP(random_state=42)
 embedding = reducer.fit_transform(X_scaled)
 
